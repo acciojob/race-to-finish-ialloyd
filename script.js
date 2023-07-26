a1ac3eb8a98e3dce51e42146d27faf19d05af8b8
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-26 15:56:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+for (let i = 0; i < 5; i++) {
+    promises.push(new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Promise ${i} resolved`);
+        }, Math.random() * 4000 + 1000);
+    }));
+}
+
+Promise.any(promises).then((result) => {
+    document.querySelector('#output').innerHTML = result;
+});
